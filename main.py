589f20c0887702a41c414809d7601e5fdcee4ac6
JamieIsMe <120991704+JamieIsMe@users.noreply.github.com>
2024-02-05 21:42:26 +0000

    BGR, RGB

--- main.py
+++ main.py
@@ -12,14 +12,11 @@ def binaryConverter(data):
 
 def encode_message(picture, data):
     image = cv2.imread(picture)
-    byteAmount = image.shape[0] * image.shape[1] * 3//8
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     index = 0
     data += "!EOF"
     binaryData = binaryConverter(data)
     dataLength = len(binaryData)
-    if dataLength > byteAmount:
-        print("Not enough bytes in the image")
-        exit(1)
     for row in image:
         for pixel in row:
             R, G, B = binaryConverter(pixel)
@@ -30,10 +27,12 @@ def encode_message(picture, data):
                     index += 1
                 if index >= dataLength:
                     break
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
 def decode_message(picture):
     image = cv2.imread(picture)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     data = ""
     for row in image:
         for pixel in row:
@@ -41,7 +40,7 @@ def decode_message(picture):
             for i in range(3):
                 current = R if i == 0 else G if i == 1 else B
                 data += current[-1]
-    test = [data[i: i+8] for i in range(0, len(data), 8)]
+    test = [data[i:i+8] for i in range(0, len(data), 8)]
     decodedData = ""
     for i in test:
         decodedData += chr(int(i, 2))
@@ -52,9 +51,10 @@ def decode_message(picture):
 
 choice = input("1 to encode, 2 to decode: ")
 if choice == "1":
-    userMessage = input("Enter a string")
-    userImage = input("Enter image name")+'.png'
-    cv2.imwrite("new.png", encode_message(userImage, userMessage))
+    userMessage = input("Enter a string: ")
+    userImage = input("Enter image name: ")+'.png'
+    cv2.imwrite("test.png", encode_message(userImage, userMessage))
 else:
-    a = decode_message("new.png")
+    encodedImage = input("Enter image to decode: ")+'.png'
+    a = decode_message(encodedImage)
     print(a)
